cf6dc011350b0ae449892c108de1f5cc4fce8c3d
Oliwier <oliwierrosiak07@gmail.com>
2026-03-05 00:34:49 +0100

    Added input focus on click in searchbar, added onFocus/onBlur action on input

--- src/components/home/aside/search/search.js
+++ src/components/home/aside/search/search.js
@@ -1,18 +1,37 @@
 import styles from './search.module.css'
 import SearchIcon from '../../../../assets/svg/searchIcon'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import CancelIcon from '../../../../assets/svg/cancelIcon'
 
 function Search(props)
 {
     const [searchValue,setSearchValue] = useState('')
 
+    const inputRef = useRef()
+
+    const searchClicked = (e)=>
+    {
+        if(e.target.closest('button')) return
+
+        inputRef.current.focus()
+    }
+
+    const inputFocus = (e) =>
+    {
+        e.target.placeholder = ``
+    }
+
+    const inputBlur = (e)=>
+    {
+        e.target.placeholder = `Wyszukaj Fakturę...`
+    }
+
     return(
-        <search className={styles.search}>
+        <search className={styles.search} onClick={searchClicked}>
 
             <SearchIcon class={styles.searchSVG} />
 
-            <input value={searchValue} onChange={e=>setSearchValue(e.target.value)} className={styles.input} placeholder='Wyszukaj Fakturę...'></input>
+            <input onFocus={inputFocus} onBlur={inputBlur} ref={inputRef} value={searchValue} onChange={e=>setSearchValue(e.target.value)} className={styles.input} placeholder='Wyszukaj Fakturę...'></input>
 
             <button className={`${styles.clearSearch} ${searchValue!==''?styles.showClearSearch:''}`} onClick={e=>setSearchValue('')}>
                 <CancelIcon class={styles.cancelIconSVG}/>
