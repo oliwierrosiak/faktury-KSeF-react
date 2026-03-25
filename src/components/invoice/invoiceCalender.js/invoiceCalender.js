1345a138d3f68554af04bc0b0388c04e26b77970
Oliwier <oliwierrosiak07@gmail.com>
2026-03-26 00:34:37 +0100

    Added exclusion of future days in calender

--- src/components/invoice/invoiceCalender.js/invoiceCalender.js
+++ src/components/invoice/invoiceCalender.js/invoiceCalender.js
@@ -10,35 +10,45 @@ function InvoiceCalender(props)
     const [month,setMonth] = useState(months[date.getMonth()])
     const [year,setYear] = useState(date.getFullYear())
 
-    const setNewMonth = (direction,e) =>
+    const [arrowDisabled,setArrowDisabled] = useState(true)
+
+    const setNewMonth = (direction) =>
     {
-        if(e.target.classList.contains(styles.arrowDisabled)) return
         if(direction === 'back')
         {
             date.setMonth(date.getMonth()-1)
             setMonth(months[date.getMonth()])
             setYear(date.getFullYear())
+            setArrowDisabled(false) 
+           
         }
         else if(direction === 'forward')
         {
+            if(arrowDisabled) return
             date.setMonth(date.getMonth()+1)
             setMonth(months[date.getMonth()])
-            setYear(date.getFullYear()) 
+            setYear(date.getFullYear())
+            const nextMonth = new Date()
+            nextMonth.setDate(nextMonth.getMonth()+1)
+            if(date.toISOString() >= nextMonth.toISOString())
+            {
+                setArrowDisabled(true)
+            }
         }
     }
 
-    const checkArrowDisabled = () =>
+    const checkDay = (day) =>
     {
+        const localDate = new Date(date)
         const currentDate = new Date()
-        const selectedDate = new Date(date)
-        selectedDate.setMonth(selectedDate.getMonth()+1)
-        if(currentDate.toISOString() <= selectedDate.toISOString())
+        localDate.setDate(day)
+        if(localDate.toISOString() <= currentDate.toISOString())
         {
-            return styles.arrowDisabled
+            return ''
         }
         else
         {
-            return ''
+            return styles.dayDisabled
         }
     }
 
@@ -47,14 +57,14 @@ function InvoiceCalender(props)
             <header className={styles.header}>
                 <h2 className={styles.header1}>Data Opłacenia</h2>
                 <div className={styles.month}>
-                    <div className={styles.arrowContainer} onClick={e=>setNewMonth('back',e)}>
+                    <div className={styles.arrowContainer} onClick={e=>setNewMonth('back')}>
                         <ArrowIcon class={`${styles.arrow} ${styles.arrowRotated}`}/>
                     </div>
 
                     <h3>{month} {year}</h3>
 
-                    <div className={styles.arrowContainer} onClick={e=>setNewMonth('forward',e)}>
-                        <ArrowIcon class={`${styles.arrow} ${checkArrowDisabled()}`}/>
+                    <div className={styles.arrowContainer} onClick={e=>setNewMonth('forward')}>
+                        <ArrowIcon class={`${styles.arrow} ${arrowDisabled?styles.arrowDisabled:''}`}/>
 
                     </div>
                 </div>
@@ -67,37 +77,37 @@ function InvoiceCalender(props)
             <div className={styles.calenderHeader}>Sob</div>
             <div className={styles.calenderHeader}>Nie</div>
 
-            <div className={styles.calenderItem}>1</div>
-            <div className={styles.calenderItem}>2</div>
-            <div className={styles.calenderItem}>3</div>
-            <div className={styles.calenderItem}>4</div>
-            <div className={styles.calenderItem}>5</div>
-            <div className={styles.calenderItem}>6</div>
-            <div className={`${styles.calenderItem} ${styles.calenderItemSelected}`}>7</div>
-            <div className={styles.calenderItem}>8</div>
-            <div className={styles.calenderItem}>9</div>
-            <div className={styles.calenderItem}>10</div>
-            <div className={styles.calenderItem}>11</div>
-            <div className={styles.calenderItem}>12</div>
-            <div className={styles.calenderItem}>13</div>
-            <div className={styles.calenderItem}>14</div>
-            <div className={styles.calenderItem}>15</div>
-            <div className={styles.calenderItem}>16</div>
-            <div className={styles.calenderItem}>17</div>
-            <div className={styles.calenderItem}>18</div>
-            <div className={styles.calenderItem}>19</div>
-            <div className={styles.calenderItem}>20</div>
-            <div className={styles.calenderItem}>21</div>
-            <div className={styles.calenderItem}>22</div>
-            <div className={styles.calenderItem}>23</div>
-            <div className={styles.calenderItem}>24</div>
-            <div className={styles.calenderItem}>25</div>
-            <div className={styles.calenderItem}>26</div>
-            <div className={styles.calenderItem}>27</div>
-            <div className={styles.calenderItem}>28</div>
-            <div className={styles.calenderItem}>29</div>
-            <div className={styles.calenderItem}>30</div>
-            <div className={styles.calenderItem}>31</div>
+            <div className={`${styles.calenderItem} ${checkDay(1)}`}>1</div>
+            <div className={`${styles.calenderItem} ${checkDay(2)}`}>2</div>
+            <div className={`${styles.calenderItem} ${checkDay(3)}`}>3</div>
+            <div className={`${styles.calenderItem} ${checkDay(4)}`}>4</div>
+            <div className={`${styles.calenderItem} ${checkDay(5)}`}>5</div>
+            <div className={`${styles.calenderItem} ${checkDay(6)}`}>6</div>
+            <div className={`${styles.calenderItem} ${checkDay(7)}`}>7</div>
+            <div className={`${styles.calenderItem} ${checkDay(8)}`}>8</div>
+            <div className={`${styles.calenderItem} ${checkDay(9)}`}>9</div>
+            <div className={`${styles.calenderItem} ${checkDay(10)}`}>10</div>
+            <div className={`${styles.calenderItem} ${checkDay(11)}`}>11</div>
+            <div className={`${styles.calenderItem} ${checkDay(12)}`}>12</div>
+            <div className={`${styles.calenderItem} ${checkDay(13)}`}>13</div>
+            <div className={`${styles.calenderItem} ${checkDay(14)}`}>14</div>
+            <div className={`${styles.calenderItem} ${checkDay(15)}`}>15</div>
+            <div className={`${styles.calenderItem} ${checkDay(16)}`}>16</div>
+            <div className={`${styles.calenderItem} ${checkDay(17)}`}>17</div>
+            <div className={`${styles.calenderItem} ${checkDay(18)}`}>18</div>
+            <div className={`${styles.calenderItem} ${checkDay(19)}`}>19</div>
+            <div className={`${styles.calenderItem} ${checkDay(20)}`}>20</div>
+            <div className={`${styles.calenderItem} ${checkDay(21)}`}>21</div>
+            <div className={`${styles.calenderItem} ${checkDay(22)}`}>22</div>
+            <div className={`${styles.calenderItem} ${checkDay(23)}`}>23</div>
+            <div className={`${styles.calenderItem} ${checkDay(24)}`}>24</div>
+            <div className={`${styles.calenderItem} ${checkDay(25)}`}>25</div>
+            <div className={`${styles.calenderItem} ${checkDay(26)}`}>26</div>
+            <div className={`${styles.calenderItem} ${checkDay(27)}`}>27</div>
+            <div className={`${styles.calenderItem} ${checkDay(28)}`}>28</div>
+            <div className={`${styles.calenderItem} ${checkDay(29)}`}>29</div>
+            <div className={`${styles.calenderItem} ${checkDay(30)}`}>30</div>
+            <div className={`${styles.calenderItem} ${checkDay(31)}`}>31</div>
         </section>
     )
 }
